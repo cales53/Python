f406c7765a5de4967e72d793c42d4c837773b695
cales53 <cales53@gmail.com>
2021-10-21 15:14:08 -0500

    data base conection

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import pyodbc
 from kivy.app import App
 from kivy.uix.button import Button
+
+
+
+server = "192.168.1.89"
+bd = "Clientes"
+usuario = "usuario"
+contrasena = "Manager21"
+try:
+    conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
+    print("Conexion exitosa")
+except:
+    print("Conexion fallida")
+
+cursor = conexion.cursor()
 
 class TestApp(App):
     def build(self):
         return Button(text='Hello World')
-
 TestApp().run()
