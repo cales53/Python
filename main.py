8d6a8f3e8c3c5f2c0252c06938e62644b7a66c67
cales53 <cales53@gmail.com>
2021-10-22 10:38:05 -0500

    conexion base de datos con pyodbc

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import tkinter as tk
+import pyodbc
 
 mywindow = tk.Tk()
 mywindow.title("Python + Tkinter GUI")
 mywindow.geometry("600x400")
 mywindow.mainloop()
+
+#conexion a base de datos
+
+server = "192.168.1.89"
+bd = "Clientes"
+usuario = "usuario"
+contrasena = "Manager21"
+try:
+    conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
+    print("Conexion exitosa")
+except:
+    print("Conexion fallida")
+
+cursor = conexion.cursor()
