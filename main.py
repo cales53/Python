593a138d91cf2c93ccd126263aea507b772a81f5
cales53 <cales53@gmail.com>
2021-10-24 09:58:24 -0500

    Ingreso gird y cambios de labels

--- main.py
+++ main.py
@@ -5,26 +5,28 @@ import pyodbc
 mywindow = tk.Tk()
 mywindow.title("Ingreso Cliente Nuevo")
 mywindow.geometry("600x400")
+mywindow.resizable(0, 0)
 
 content = ttk.Frame(mywindow)
 frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
 namelbl = ttk.Label(content, text="Name")
 name = ttk.Entry(content)
 
+botone = tk.Button(mywindow, text="Enviar")
+botone.place(x=30, y=350)
+botonc = ttk.Button(text="Cancelar")
+botonc.place(x=500, y=350)
 
-etiqueta_nombre = ttk.Label(text="Nombre del cliente")
-etiqueta_nombre.place(x=30, y=15)
-caja_nombre = ttk.Entry()
-caja_nombre.place(x=180, y=15, width=250)
-etiqueta_empresa = ttk.Label(text="Nombre de la empresa")
-etiqueta_empresa.place(x=30, y=40)
-caja_empresa = ttk.Entry()
-caja_empresa.place(x=180, y=40, width=250)
+tk.Label(mywindow, text="Nombre del cliente").grid(row=1, column=1)
+tk.Label(mywindow, text="Nombre de la empresa").grid(row=2, column=1)
 
-boton = ttk.Button(text="Enviar")
-boton.place(x=30, y=350)
-boton = ttk.Button(text="Cancelar")
-boton.place(x=500, y=350)
+# Create the entry objects using master
+cnombre = tk.Entry(mywindow)
+cempresa = tk.Entry(mywindow)
+ 
+# Pack them using grid
+cnombre.grid(row=1, column=2)
+cempresa.grid(row=2, column=2)
 
 mywindow.mainloop()
 
