a1ce4e8fb677df386e6eb2e1266daf4bd169b7ae
cales53 <cales53@gmail.com>
2021-10-25 22:25:53 -0500

    Pruebas segunda pantalla

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import pyodbc
+from tkinter.messagebox import showinfo
 
 server = "192.168.1.89"
 bd = "Clientes"
@@ -16,9 +17,13 @@ mywindow = tk.Tk()
 mywindow.title("Ingreso Cliente Nuevo")
 mywindow.resizable(1, 1)
 
-botone = ttk.Button(mywindow, text="Enviar")
+botone = ttk.Button(text="Enviar")
 botone.grid(column=1, row=9, sticky=tk.SW, padx=5, pady=5)
-botonc = ttk.Button(text="Cancelar")
+botonc = ttk.Button(
+    mywindow, 
+    text="Cancelar",
+    command=lambda: mywindow.quit()
+)
 botonc.grid(column=2, row=9, sticky=tk.SE, padx=5, pady=5)
 
 tk.Label(mywindow, text="Nombre del cliente").grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
@@ -51,3 +56,40 @@ cursor.commit()
 cursor.close()
 
 conexion.close()
+
+'''def cant():
+    mywindow2 = tk.Tk()
+    mywindow2.title("Ingreso Cliente Nuevo")
+    mywindow2.resizable(1, 1)
+
+    def cant_changed(event):
+        msg = f'You selected {cont_cant_cb.get()}!'
+        showinfo(title='Result', message=msg)
+
+    # month of year
+    cantidad = ('1', '2', '3', '4', '5', '6',
+            '7', '8', '9', '10', '11', '12')
+
+    label = tk.Label(mywindow2, text="Cuantos concentradores").grid(row=5, column=1, sticky=tk.W, padx=5, pady=5)
+
+    # create a combobox
+    selected_cantidad = tk.StringVar()
+
+    cont_cant_cb = ttk.Combobox(mywindow2, textvariable=selected_cantidad, width = 2)
+    cont_cant_cb.grid(column=1, row=6, sticky=tk.SW, padx=5, pady=5)
+    cont_cant_cb['values'] = cantidad
+    cont_cant_cb['state'] = 'readonly'  # normal
+
+    cont_cant_cb.bind('<<ComboboxSelected>>', cant_changed)
+
+
+    label = tk.Label(mywindow2, text="Cuantos concentradores").grid(row=7, column=1, sticky=tk.W, padx=5, pady=5)
+
+    control_cant_cb = ttk.Combobox(mywindow2, textvariable=selected_cantidad, width = 2)
+    control_cant_cb.grid(column=1, row=8, sticky=tk.SW,padx=5, pady=5)
+    control_cant_cb['values'] = cantidad
+    control_cant_cb['state'] = 'readonly'  # normal
+
+    control_cant_cb.bind('<<ComboboxSelected>>', cant_changed)
+
+    mywindow2.mainloop()'''
