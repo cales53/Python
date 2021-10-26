eff26f610bc684917f93172df2704a4759792be9
cales53 <cales53@gmail.com>
2021-10-25 21:57:01 -0500

    adicion pestaña de cantidades

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 import pyodbc
-from tkinter.messagebox import showinfo
+
+server = "192.168.1.89"
+bd = "Clientes"
+usuario = "usuario"
+contrasena = "Manager21"
+try:
+    conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
+    print("Conexion exitosa")
+except:
+    print("Conexion fallida")
 
 mywindow = tk.Tk()
 mywindow.title("Ingreso Cliente Nuevo")
@@ -29,50 +38,9 @@ cempresa.grid(row=2, column=2, sticky=tk.W, padx=5, pady=5)
 ccargo.grid(row=3, column=2, sticky=tk.W, padx=5, pady=5)
 cphone.grid(row=4, column=2, sticky=tk.W, padx=5, pady=5)
 
-def cant_changed(event):
-    msg = f'You selected {cont_cant_cb.get()}!'
-    showinfo(title='Result', message=msg)
-
-# month of year
-cantidad = ('1', '2', '3', '4', '5', '6',
-        '7', '8', '9', '10', '11', '12')
-
-label = tk.Label(mywindow, text="Cuantos concentradores").grid(row=5, column=1, sticky=tk.W, padx=5, pady=5)
-
-# create a combobox
-selected_cantidad = tk.StringVar()
-
-cont_cant_cb = ttk.Combobox(mywindow, textvariable=selected_cantidad, width = 2)
-cont_cant_cb.grid(column=1, row=6, sticky=tk.SW, padx=5, pady=5)
-cont_cant_cb['values'] = cantidad
-cont_cant_cb['state'] = 'readonly'  # normal
-
-cont_cant_cb.bind('<<ComboboxSelected>>', cant_changed)
-
-
-label = tk.Label(mywindow, text="Cuantos concentradores").grid(row=7, column=1, sticky=tk.W, padx=5, pady=5)
-
-control_cant_cb = ttk.Combobox(mywindow, textvariable=selected_cantidad, width = 2)
-control_cant_cb.grid(column=1, row=8, sticky=tk.SW,padx=5, pady=5)
-control_cant_cb['values'] = cantidad
-control_cant_cb['state'] = 'readonly'  # normal
-
-control_cant_cb.bind('<<ComboboxSelected>>', cant_changed)
-
 mywindow.mainloop()
 
 #Conexion a base de datos
-
-server = "192.168.1.89"
-bd = "Clientes"
-usuario = "usuario"
-contrasena = "Manager21"
-try:
-    conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
-    print("Conexion exitosa")
-except:
-    print("Conexion fallida")
-
 
 cursor = conexion.cursor()
 cursor.execute("select * from persona;")
