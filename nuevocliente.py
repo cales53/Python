daf666ccc09decf2fb1ab61e872f18cc91ae2bb9
cales53 <cales53@gmail.com>
2021-10-29 12:35:24 -0500

    creacion root window

---
+++ nuevocliente.py
@@ -0,0 +1,110 @@
+import tkinter as tk
+from tkinter import Entry, StringVar, ttk
+from tkinter.constants import CHAR
+from typing import Text
+import pyodbc
+from configuracion import server, bd, usuario, contrasena
+from cantidades import openNewWindow
+
+
+#from tkinter.messagebox import showinfo
+conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
+
+def consulta(cnombre,cempresa,ccargo,cphone):
+    cursor = conexion.cursor()
+    consulta = "Insert into persona(nombre,ntelefono,empresa,cargo) values (?,?,?,?)"
+    cursor.execute(consulta,cnombre,cphone,cempresa,ccargo)
+    cursor.execute("select * from persona;")
+    personas = cursor.fetchall()
+    for persona in personas:
+        print(persona)    
+    cursor.commit()
+    cursor.close()
+    # creacion de una segunda ventana para dar continuacion al ingreso de cantidades
+    openNewWindow()
+    
+def WindowCliente():  
+    mywindow = tk.Tk()
+    mywindow.title("Nuevo Cliente")
+    mywindow.resizable(1, 1)
+
+    botonc = ttk.Button(
+        mywindow, 
+        text="Cancelar",
+        command=lambda: mywindow.quit()
+    )
+    botonc.grid(column=2, row=9, sticky=tk.SE, padx=5, pady=5)
+
+    tk.Label(mywindow, text="Nombre del cliente").grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+    tk.Label(mywindow, text="Nombre de la empresa").grid(row=2, column=1, sticky=tk.W, padx=5, pady=5)
+    tk.Label(mywindow, text="Cargo").grid(row=3, column=1, sticky=tk.W, padx=5, pady=5)
+    tk.Label(mywindow, text="Celular").grid(row=4, column=1, sticky=tk.W, padx=5, pady=5)
+
+    # Crear las cajas de ingreso
+    cnombre = tk.Entry(mywindow)
+    cempresa = tk.Entry(mywindow)
+    ccargo = tk.Entry(mywindow)
+    cphone = tk.Entry(mywindow)
+    # cajones en grid
+    cnombre.grid(row=1, column=2, sticky=tk.W, padx=5, pady=5)
+    cempresa.grid(row=2, column=2, sticky=tk.W, padx=5, pady=5)
+    ccargo.grid(row=3, column=2, sticky=tk.W, padx=5, pady=5)
+    cphone.grid(row=4, column=2, sticky=tk.W, padx=5, pady=5)
+
+    botone = ttk.Button(
+        mywindow, 
+        text="Enviar", 
+        command=lambda:consulta(cnombre.get(),cempresa.get(),ccargo.get(),cphone.get())
+    )
+    botone.grid(column=1, row=9, sticky=tk.SW, padx=5, pady=5)
+
+    mywindow.mainloop()
+
+#Conexion a base de datos
+
+'''cursor = conexion.cursor()
+cursor.execute("select * from persona;")
+personas = cursor.fetchall()
+for persona in personas:
+    print(persona)    
+cursor.commit()
+cursor.close()
+
+conexion.close()'''
+
+'''def cant():
+    mywindow2 = tk.Tk()
+    mywindow2.title("Ingreso Cliente Nuevo")
+    mywindow2.resizable(1, 1)
+
+    def cant_changed(event):
+        msg = f'You selected {cont_cant_cb.get()}!'
+        showinfo(title='Result', message=msg)
+
+    # month of year
+    cantidad = ('1', '2', '3', '4', '5', '6',
+            '7', '8', '9', '10', '11', '12')
+
+    label = tk.Label(mywindow2, text="Cuantos concentradores").grid(row=5, column=1, sticky=tk.W, padx=5, pady=5)
+
+    # create a combobox
+    selected_cantidad = tk.StringVar()
+
+    cont_cant_cb = ttk.Combobox(mywindow2, textvariable=selected_cantidad, width = 2)
+    cont_cant_cb.grid(column=1, row=6, sticky=tk.SW, padx=5, pady=5)
+    cont_cant_cb['values'] = cantidad
+    cont_cant_cb['state'] = 'readonly'  # normal
+
+    cont_cant_cb.bind('<<ComboboxSelected>>', cant_changed)
+
+
+    label = tk.Label(mywindow2, text="Cuantos concentradores").grid(row=7, column=1, sticky=tk.W, padx=5, pady=5)
+
+    control_cant_cb = ttk.Combobox(mywindow2, textvariable=selected_cantidad, width = 2)
+    control_cant_cb.grid(column=1, row=8, sticky=tk.SW,padx=5, pady=5)
+    control_cant_cb['values'] = cantidad
+    control_cant_cb['state'] = 'readonly'  # normal
+
+    control_cant_cb.bind('<<ComboboxSelected>>', cant_changed)
+
+    mywindow2.mainloop()'''
