b3b6381f411ff7b18ba0cfd61868ca21f3f62831
cales53 <cales53@gmail.com>
2021-11-09 12:30:19 -0500

    Reduccion de espacios

--- configuracion.py
+++ configuracion.py
@@ -11,9 +11,11 @@ try:
     print("Conexion exitosa")
 except:
     print("Conexion fallida")
+    
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect('192.168.1.2', 22, 'admin', '%it$2018')
 entrada, salida, error = ssh_client.exec_command('ls -la')
 print(salida.read())
+print(error.read())
 ssh_client.close()
