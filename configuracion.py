7d51640aacef69d492559ab793752955228c7c32
cales53 <cales53@gmail.com>
2021-11-12 12:55:30 -0500

    Actualizacion archivo config

--- configuracion.py
+++ configuracion.py
@@ -1,5 +1,7 @@
-import pyodbc
+import time
 import paramiko
+from getpass import getpass
+import pyodbc
 
 server = "192.168.1.89"
 bd = "Clientes"
@@ -10,12 +12,12 @@ try:
     conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
     print("Conexion exitosa")
 except:
-    print("Conexion fallida")
-    
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect('192.168.1.2', 22, 'admin', '%it$2018')
-entrada, salida, error = ssh_client.exec_command('ls -la')
-print(salida.read())
-print(error.read())
-ssh_client.close()
+    print("Conexion fallida a base de datos")
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect('190.147.177.209', '222', username='cales53@msn.com', password='carlosvega4')
+        stdin,stdout,stderr = client.exec_command('dir')
+        conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
+    except paramiko.AuthenticationException as e:
+        print('Autenticacion fallida')
