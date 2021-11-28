46a2295bfb584b8ce077c66daa02da1f3fe2d533
cales53 <cales53@gmail.com>
2021-11-27 21:41:06 -0500

    Pruebas PDF

--- prueba.py
+++ prueba.py
@@ -1,18 +1,20 @@
+from reportlab.lib import fonts
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4, letter
 import itertools
 from random import randint
 from statistics import mean
 from reportlab.pdfbase import pdfmetrics, ttfonts
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.units import cm
+from reportlab.lib.styles import getSampleStyleSheet
+
 pdfmetrics.registerFont(ttfonts.TTFont("Arial", "Fuentes/arial.ttf"))
 
+my_text = "<font fontName=Arial size=12> De conformidad con su solicitud y atendiendo a la información y necesidades topológicas específicas suministradas por ustedes, nos permitimos presentar la siguiente:</font>"
 
-w, h = letter
-c = canvas.Canvas("hola-mundo.pdf", pagesize=letter)
-text = c.beginText(50, h - 50)
-text.setFont("Arial", 12)
-text.textLines("De conformidad con su solicitud y atendiendo a la información y necesidades topológicas específicas suministradas por ustedes, nos permitimos presentar la siguiente: ")
-text.setTextOrigin(50,50)
-c.drawText(text)
-c.showPage()
-c.save()
+doc = SimpleDocTemplate("example_flowable.pdf",pagesize=A4,
+                        rightMargin=2*cm,leftMargin=2*cm,
+                        topMargin=2*cm,bottomMargin=2*cm)
+
+doc.build([Paragraph(my_text.replace("\n", "<br />"), getSampleStyleSheet()['Normal']),])
