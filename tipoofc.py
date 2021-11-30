49048e13856af450745fbcbe92ba4827402ee4a2
cales53 <cales53@gmail.com>
2021-11-29 21:19:08 -0500

    insertar tipo OFC db

--- tipoofc.py
+++ tipoofc.py
@@ -5,13 +5,20 @@ from configuracion import server, bd, usuario, contrasena
 from cantidades import openNewWindow
 conexion = pyodbc.connect("DRIVER={ODBC Driver 11 for SQL Server}; SERVER="+server+";DATABASE="+bd+";UID="+usuario+";PWD="+contrasena)
 
+
+def tipo_changed(event):
+    cursor = conexion.cursor()
+    consulta="insert into oferta(tipo) values (?);"
+    cursor.execute(consulta,'REPUESTOS')
+    cursor.close()
+
 def tipoWindow(id, nombre, empresa, cargo, phone):
     tipo= tk.Tk()
     tipo.title("Cantidades")
     tipo.resizable(1, 1)
     tipo.minsize(250,100)
     selected_tipo = tk.StringVar()
-    cantidades = ('NUEVOS SUMINISTROS', 'REPUESTOS')
+    tipoofc = ('NUEVOS SUMINISTROS', 'REPUESTOS')
     cursor = conexion.cursor()
     consulta = "update persona set ntelefono = ? , nombre = ?, cargo = ?, empresa = ? where id = ?;"
     cursor.execute(consulta, phone, nombre, cargo, empresa, id)
@@ -19,7 +26,7 @@ def tipoWindow(id, nombre, empresa, cargo, phone):
     cursor.close()
 
     tipo_cb = ttk.Combobox(tipo, textvariable=selected_tipo,width="18")
-    tipo_cb['values'] = cantidades
+    tipo_cb['values'] = tipoofc
     tipo_cb['state'] = 'readonly'  # normal
     tipo_cb.grid(row=1, column=2)
     tipo_cb.bind('<<ComboboxSelected>>', tipo_changed)
@@ -32,6 +39,3 @@ def tipoWindow(id, nombre, empresa, cargo, phone):
         command=lambda:openNewWindow()
     )
     botone.grid(column=1, row=2, sticky=tk.SW, padx=5, pady=5)
-
-def tipo_changed(event):
-    pass
